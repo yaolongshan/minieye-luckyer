43c56c4352ab7e3b20e7f45936d826c122de3513
yls <1677367467@qq.com>
2022-01-10 15:37:41 +0800

    图片开启缓存

--- controllers/api_images.go
+++ controllers/api_images.go
@@ -9,5 +9,7 @@ import (
 func ApiImagesPreview(c *gin.Context) {
 	imageName := c.Param("name")
 	path := fmt.Sprintf("%v/%v/%v", conf.Conf.RootPath, "images", imageName)
+	//强制缓存
+	c.Header("Cache-Control", "private, max-age=86400")
 	c.File(path)
 }
